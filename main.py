5b0d8c585cc18e576bbb7964009f413cad5af269
vivy-c <cahyanivivy@gmail.com>
2024-01-17 23:34:50 +0700

    [IMP] set value for param

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 
 class Player:
     #__init__ mrpkn salah satu duder method dari python spya otomatis dijalankan
-    def __init__(self, health):
-        self.x = health
+    # setiap parameter yg ada wajib di set valuenya kalo ngga nanti error missing argument, jadi perlu set default valuenya
+    def __init__(self, health = 101):
+        self.health = health
         # tambah f pada string untuk memunculkan nilai x
-        print(f"player created, x={self.x}")
+        print(f"player created, health = {self.health}")
 
     def attack(self):
         print("attacking!")
-player = Player(100)
+player = Player(201) # sama dengan -> player = Player(health=201)
 # player.attack()
