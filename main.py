c3fa671741ceea5a0f74958f86fe1495e9b98392
vivy-c <cahyanivivy@gmail.com>
2024-01-17 23:29:59 +0700

    [IMP] param usage with print and new attr to store value

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 
 class Player:
     #__init__ mrpkn salah satu duder method dari python spya otomatis dijalankan
-    def __init__(self):
-        print("player created")
+    def __init__(self, health):
+        self.x = health
+        # tambah f pada string untuk memunculkan nilai x
+        print(f"player created, x={self.x}")
 
     def attack(self):
         print("attacking!")
-player = Player()
+player = Player(100)
 # player.attack()
