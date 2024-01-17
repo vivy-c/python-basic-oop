613370b8c92f8b8f552cbb51485d6a4a499de07e
vivy-c <cahyanivivy@gmail.com>
2024-01-17 23:46:10 +0700

    [IMP] remove unused code and calculate player energy

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 
 class Player:
-    #__init__ mrpkn salah satu duder method dari python spya otomatis dijalankan
-    # setiap parameter yg ada wajib di set valuenya kalo ngga nanti error missing argument, jadi perlu set default valuenya
-    def __init__(self, health = 101, energy = 100): # ini disebut onstructor
+    def __init__(self, health = 101, energy = 100):
         self.health = health
         self.energy = energy
-        # tambah f pada string untuk memunculkan nilai x
         print(f"player created")
 
-    def attack(self):
+    def attack(self, damage = 1):
+        self.energy -= damage  # self.energy = self.emergy - damage
         print("attacking!")
-player = Player(201) # sama dengan -> player = Player(health=201)
-#dunder method untuk mereturn object isi dari attr
+player = Player()
+player.attack()
+player.attack(damage=34)
+
 print(player.__dict__)
-# player.attack()
