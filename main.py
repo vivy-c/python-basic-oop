c90589756d0c5053ddae4315349447067af13021
vivy-c <cahyanivivy@gmail.com>
2024-01-19 00:32:46 +0700

    [FEAT] boolean method simple usage

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ class Player:
     def attack(self, target, damage = 1 ):
         target.health -= damage
         self.energy -= damage  # self.energy = self.emergy - damage
+        if target.is_attacked():
+            self.health -= target.damage
+
         print(f"monster health {target.health} left, your energy : {self.energy} left")
 class Monster:
     def __init__(self, health = 200):
         self.health = health # dinamis
         self.health_init = self.health
-
-    def attack(self):
-        if self.health < self.health_init:
-            print("siap serang balik")
-        else :
-            print("tidur")
+        self.damage = 10
+    # made monster attack to boolean method
+    def is_attacked(self):
+        return self.health < self.health_init
 
 player1 = Player()
 player2 = Player()
@@ -26,5 +27,4 @@ dragon = Monster(health=500)
 player1.attack( target=dragon,damage=20 )
 player2.attack( target=dragon,damage=30 )
 
-dragon.attack()
 
