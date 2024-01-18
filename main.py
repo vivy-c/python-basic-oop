cac6dc228b3b62ed45a59955adfa3dba91c99204
vivy-c <cahyanivivy@gmail.com>
2024-01-18 23:26:02 +0700

    [IMP] simple method usage

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-#OBJECT
-#access modifier
-# __ --> private access
-# _ --> protected access
+
 class Player:
-    #inner class
-    #attribute
-    name = "joni" # public access
-    __salary = 10_000_000_000
-    #method
-    def get_salary(self):
-        return self.__salary
+    #__init__ mrpkn salah satu duder method dari python spya otomatis dijalankan
+    def __init__(self):
+        print("player created")
 
-#outter class
-#outter class bisa akses attr name karena name itu public
-# print(Player)
+    def attack(self):
+        print("attacking!")
 player = Player()
-print(f"nama {player.name} berhasil dibuat")
-
-player.name = "vivy cahyani"
-print(f"{player.name} dalah nama baru")
-player.salary = 30_000_000_000 # salary = attr baru & != attr __salary
-print(player.salary)
-print(player.get_salary())
+# player.attack()
