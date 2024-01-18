306a744df4d1e138895f66cea6e7e8534c7d1921
vivy-c <cahyanivivy@gmail.com>
2024-01-19 00:03:57 +0700

    [CHORE] add proper print and remove unused code

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ class Player:
     def __init__(self, health = 101, energy = 100):
         self.health = health
         self.energy = energy
-        print(f"player created")
 
     def attack(self, monster, damage = 1 ):
         monster.health -= damage
@@ -12,10 +11,11 @@ class Player:
 class Monster:
     def __init__(self, health):
         self.health = health
-        print("Monster Created")
 
-player = Player()
-monster = Monster(health=1000)
-player.attack( monster,damage=80 )
-print(monster.__dict__)
+player1 = Player()
+player2 = Player()
+
+dragon = Monster(health=1000)
+player1.attack( monster=dragon,damage=80 )
+player2.attack( monster=dragon,damage=80 )
 
